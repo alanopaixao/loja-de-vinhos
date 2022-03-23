1e4af0f9d65de75fe30f3aaa6ac9d84975c3a52d
Alan Paixão <alanpaixao2@gmail.com>
2022-03-23 00:35:09 -0300

    animation + multiple pages

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-//setup date
-<<<<<<< HEAD
-const date = document.getElementById('date');
-if (date) {
-  date.innerHTML = new Date().getFullYear();
-}
+const date = (document.getElementById("date").innerHTML = new Date().getFullYear());
 // setup nav
 const navBtn = document.getElementById('nav-btn');
 const navbar = document.getElementById('navbar');
@@ -14,17 +9,10 @@ if (navBtn) {
     navbar.classList.add('showNav');
   });
 }
-=======
-const date = (document.getElementById("date").innerHTML = new Date().getFullYear());
-
-const navBtn = document.getElementById("nav-btn");
-const navbar = document.getElementById("navbar");
-const navClose = document.getElementById("nav-close");
->>>>>>> beefe30 (first commit)
-
+// close nav
 if (navClose) {
   navClose.addEventListener('click', () => {
     navbar.classList.remove('showNav');
   });
 }
-// close nav
+
